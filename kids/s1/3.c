c8b1a83fc1e79736d3b79cd76293f3e8919cf6fd
soundratteddy <46959867+soundratteddy@users.noreply.github.com>
2019-02-26 16:23:55 +0530

    Create 3.c

---
+++ kids/s1/3.c
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+ {
+char ch;
+scanf("%c",&ch);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='u'||ch=='o'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+{
+  printf("vowel");
+}
+else
+{
+printf("constant");
+}
+return 0;
+}

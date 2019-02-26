b4c93c04fb450b2e0ff4eb0b144cb203d4f81b4f
soundratteddy <46959867+soundratteddy@users.noreply.github.com>
2019-02-26 16:13:25 +0530

    Create 1.c

---
+++ kids/s1/1.c
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+ {
+int a;
+scanf("%d",&a);
+if(a<0)
+{
+  printf("negative");
+}
+if(a>0)
+{
+printf("postive");
+}
+if(a==0)
+{
+  printf("zero");
+}
+return 0;
+}

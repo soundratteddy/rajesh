53477dda8087457ec20dc269fa1b42bc87c58c02
soundratteddy <46959867+soundratteddy@users.noreply.github.com>
2019-03-25 10:32:15 +0530

    Create pr3.c

---
+++ pr3.c
@@ -0,0 +1,15 @@
+include<stdio.h>
+int main()
+{
+int rev=0,n,a,i,sum=1;
+scanf("%d",&n);
+int temp=n;
+for(i=1;temp!=0;i++)
+{
+    a=temp%10;
+    rev=rev*10+a;
+    temp=temp/10;
+}
+printf("%d",rev);
+return 0;
+}

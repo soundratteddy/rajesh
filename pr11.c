ee12884a60c6f0e2b6d86091adfc877c6080d12d
soundratteddy <46959867+soundratteddy@users.noreply.github.com>
2019-03-25 09:59:37 +0530

    Create pr11.c

---
+++ pr11.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+	#include<conio.h>
+	void main()
+	{
+	char a[100];
+	int i;
+	printf("enter the day");
+	scanf("%s",&a);
+	if(a[i]=='s')
+	{
+	printf("yes");
+	}
+	else
+	{
+	printf("no");
+	}
+	getch();
+	}
